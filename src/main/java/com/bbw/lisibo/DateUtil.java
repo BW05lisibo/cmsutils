e1c745bb1002c782a2ce8ceba5dc301d77893029
BW05lisibbo <918934423@qq.com>
2019-09-06 20:09:13 +0800

    日期工具类

--- src/main/java/com/bbw/lisibo/DateUtil.java
+++ src/main/java/com/bbw/lisibo/DateUtil.java
@@ -1,5 +1,7 @@
 package com.bbw.lisibo;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -12,7 +14,7 @@ public class DateUtil {
 	/**
 	 * 计算年龄
 	 */
-	public int getAge(Date birthday){
+	public static int getAge(Date birthday){
 		Calendar instance=Calendar.getInstance();
 		instance.setTime(birthday);
 		
@@ -34,5 +36,79 @@ public class DateUtil {
 			age=age-1;
 		}
 		return age;
+	}
+	
+	
+	/**
+	 * 
+	 * @param futureDate
+	 * @return
+	 */
+	public int getRemainDays(Date futureDate) {
+		return (int)(futureDate.getTime()-new Date().getTime())/1000/3600/24;	
+	}
+	
+	/**
+	 * 3.4.4判断是否为当天
+	 * @param date
+	 * @return
+	 */
+	public static boolean isToday(Date date) {
+		
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
+		String dateStr = simpleDateFormat.format(date);
+		String todayStr = simpleDateFormat.format(new Date());
+		return dateStr.equals(todayStr);
+		
+	}
+	
+	
+	/**
+	 * 3.4.4判断是否为当月
+	 * @param date
+	 * @return
+	 */
+	public static boolean isCurrentMonth(Date date) {
+		
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMM");
+		String dateStr = simpleDateFormat.format(date);
+		String todayStr = simpleDateFormat.format(new Date());
+		return dateStr.equals(todayStr);
+		
+	}
+	
+	/**
+	 * 3.4.7给定时间对象，初始化到该月初的1日0时0分0秒0毫秒
+	 * @return
+	 * @throws ParseException 
+	 */
+	public static Date getMonthStart(Date date) throws ParseException {
+		SimpleDateFormat format = new SimpleDateFormat("yyyyMM");
+		String string = format.format(date);//199809
+		String monthStart = string + "01 00:00:00";
+		format = new SimpleDateFormat("yyyyMMdd HH:mm:ss");
+		return format.parse(monthStart);
+	}
+	
+	
+	/**
+	 * 3.4.7给定时间对象，初始化到该月末的最后一秒  比如 2019/11/30 23:59:59 
+	 * @return
+	 * @throws ParseException 
+	 */
+	public static Date getMonthEnd(Date date) throws ParseException {
+		
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		calendar.add(Calendar.MONTH, 1);
+		calendar.set(Calendar.DAY_OF_MONTH, 1);
+		calendar.set(Calendar.HOUR_OF_DAY,0);
+		calendar.set(Calendar.MINUTE, 0);
+		//calendar.set(Calendar.HOUR, 0);
+		
+		calendar.set(Calendar.SECOND, 0);
+		calendar.add(Calendar.SECOND, -1);
+		return calendar.getTime(); 
+		
 	}
 }
